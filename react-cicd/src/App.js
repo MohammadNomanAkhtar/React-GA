1b9dde800e5403c00679e40290613545111c85ba
MohammadNomanAkhtar <smnomanakhtar@gmail.com>
2024-09-06 07:44:30 -0700

    push second times

--- react-cicd/src/App.js
+++ react-cicd/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Updated code by local machine
+          Edit <code>src/App.js</code> and save to reload file .
         </p>
         <a
           className="App-link"
